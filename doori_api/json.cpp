d2e5aa2d56c27ad59cb4c3834ed6eb2273189f91
이재성 <b532882@gmail.com>
2020-04-27 23:25:01 +0900

    json 2차

--- doori_api/json.cpp
+++ doori_api/json.cpp
@@ -6,34 +6,54 @@
 
 namespace doori{
 
-    auto Json::operator[](const char *jsonkey) -> Json & {
-        return <#initializer#>;
+    auto Json::operator[](const std::string &jsonkey) -> Json_value & {
+        return mFactors[jsonkey];
     }
 
     auto Json::clear() -> void {
-
+        mFactors.clear();
     }
 
     auto Json::toString() -> std::string {
-        return std::__cxx11::string();
+        for(auto &i:mFactors)
+        {
+            switch(i.second.TYPE)
+            {
+                case Json_value::BOOL:
+            }
+        }
     }
 
     auto Json::fromString(const char *json) -> bool {
         return false;
     }
 
-    auto Json::append(const char *jsonkey, const Json &json) -> void {
-        std::unordered_map<const char*, Json>
-        mFactors.push_back()
+    auto Json::append(const char *jsonKey, const Json_value &jsonValue) -> void {
+        mFactors[jsonKey] = jsonValue;
     }
 
-    auto Json::Json_value::set(int32_t value) -> void {
-        Json::Json_value::TYPE = INT32;
-        Json::Json_value::mInt = value;
+    auto Json_value::set(int32_t value) -> void {
+        TYPE = INT32S;
+        mInt = value;
     }
 
-    auto Json::Json_value::set(std::string value) -> void {
-        Json::Json_value::TYPE = Json::Json_value::INT32;
-        Json::Json_value::mInt = value;
+    auto Json_value::set(std::string value) -> void {
+        TYPE = INT32S;
+        mStr = value;
+    }
+
+    auto Json_value::set(float value) -> void {
+        TYPE = FLOAT;
+        mFloat = value;
+    }
+
+    auto Json_value::set(bool value) -> void {
+        TYPE = BOOL;
+        mBool = value;
+    }
+
+    auto Json_value::set(Json value) -> void {
+        TYPE = JSON;
+        mJson = std::make_unique<Json>(value);
     }
 }
--- doori_api/json.h
+++ doori_api/json.h
@@ -6,6 +6,7 @@
 #define DOORI_API_JSON_H
 
 
+
 #include <cstdint>
 #include <string>>
 #include <memory>
@@ -14,37 +15,40 @@
 
 
 namespace doori{
+    class Json;
+
+    class Json_value{
+    public:
+        enum {
+            NIL,
+            INT32S,
+            STRING,
+            FLOAT,
+            BOOL,
+            JSON
+        }TYPE;
+        auto set(int32_t value) -> void;
+        auto set(std::string value) -> void;
+        auto set(float value) -> void;
+        auto set(bool value) -> void;
+        auto set(Json value) -> void;
+    private:
+        int32_t     mInt;
+        std::string mStr;
+        float       mFloat;
+        bool        mBool;
+        std::shared_ptr<Json> mJson;
+    };
+
     class Json {
     public:
-        class Json_value{
-        public:
-            enum TYPE{
-                NIL,
-                INT32,
-                STRING,
-                FLOAT,
-                BOOL,
-                JSON
-            };
-            auto set(int32_t value) -> void;
-            auto set(std::string value) -> void;
-            auto set(float value) -> void;
-            auto set(bool value) -> void;
-        private:
-            int32_t     mInt;
-            std::string mStr;
-            float       mFloat;
-            bool        mBool;
-        };
-        auto append(const char* jsonkey, const Json& json) -> void;
-        auto operator[](const char* jsonkey) -> Json&;
+        auto append(const char* jsonKey, const Json_value &jsonValue) -> void;
+        auto operator[](const std::string &jsonkey) -> Json_value&;
         auto toString() -> std::string;
         auto fromString(const char *json) -> bool;
         auto clear() -> void;
     private:
-        auto set(const Json_value& value) -> void;
-        auto link( std::unordered_map<const char*, Json> factor ) -> void;
-        std::vector< std::unordered_map<const char*, Json> > mFactors;
+        std::unordered_map<std::string, Json_value> mFactors;
     };
 }
 #endif //DOORI_API_JSON_H
