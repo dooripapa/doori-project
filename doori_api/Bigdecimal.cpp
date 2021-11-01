c7a89f20f15e5c676a7b9e9fcbbb8f84ebec957e
dooripapa <b532882@hotmail.com>
2021-11-01 22:10:11 +0900

    Big decimal 클래스 추가

---
+++ doori_api/Bigdecimal.cpp
@@ -0,0 +1,192 @@
+//
+// Created by jaeseong on 21. 11. 1..
+//
+
+#include "Bigdecimal.h"
+
+namespace doori {
+
+Bigdecimal::Bigdecimal() {
+
+}
+
+Bigdecimal::Bigdecimal(std::string value) {
+    coreValue = value;
+}
+
+Bigdecimal::Bigdecimal(const Bigdecimal &rhs) {
+    if(this==&rhs)
+        return;
+
+    copyFrom(rhs);
+    return;
+}
+
+Bigdecimal::Bigdecimal(Bigdecimal &&rhs) {
+    if(this==&rhs)
+        return;
+
+    copyFrom(std::move(rhs));
+    return;
+}
+
+auto Bigdecimal::muliplicated(std::string value, char c, uint zeroCharCnt) -> std::string {
+    int i=value.size()-1;
+    int iValue = 0;
+    int jValue = c-'0';
+    int multiValue = 0;
+    char upValueC='0';
+    std::forward_list<char> ret;
+    std::string itos;
+
+    for(int i=0;i<zeroCharCnt;i++) {
+        ret.push_front('0');
+    }
+
+    for(;;--i) {
+        if(i<0) {
+            if(upValueC>'0')
+                ret.push_front(upValueC);
+            break;
+        }
+
+        iValue = value[i]-'0';
+
+        multiValue = iValue*jValue;
+
+        multiValue+=(upValueC-'0');
+
+        itos = std::to_string(multiValue);
+        if(itos.length()>1){
+            upValueC = itos[0];
+            ret.push_front(itos[1]);
+        }
+        else {
+            upValueC = '0';
+            ret.push_front(itos[0]);
+        }
+    }
+    return std::string(ret.begin(), ret.end());
+}
+
+auto Bigdecimal::operator+(const Bigdecimal &rhs) -> Bigdecimal& {
+    std::forward_list<char> ret;
+    int i=coreValue.size()-1;
+    int j=rhs.coreValue.size()-1;
+
+    std::string itos;
+    int iValue, jValue, upValue;
+    upValue=0;
+    for(;; --i, --j) {
+        if(i<0 && j<0) {
+            if(upValue>0)
+                ret.push_front('1');
+            break;
+        }
+
+        if(i<0)
+            iValue = 0;
+        else
+            iValue = coreValue[i]-'0';
+
+        if(j<0)
+            jValue = 0;
+        else
+            jValue = rhs.coreValue[j]-'0';
+
+        if(upValue>0)
+            iValue+=1;
+
+        itos = std::to_string(iValue + jValue);
+        if( itos.length() > 1) {
+            upValue=1;
+            ret.push_front(itos[1]);
+        }
+        else {
+            upValue=0;
+            ret.push_front(itos[0]);
+        }
+        iValue=jValue=0;
+    }
+    coreValue = std::string(ret.begin(),ret.end());
+}
+
+auto Bigdecimal::operator*(const Bigdecimal &rhs) -> Bigdecimal & {
+    std::string sI;
+    std::string sBefore;
+    std::string sSum="1";
+    std::string sSumRet="0";
+    std::string sRet="1";
+
+    int zeroCharCnt=0;
+    std::list<std::string> stringList;
+
+    for_each(rhs.coreValue.begin(), rhs.coreValue.end(), [&](int i){
+        auto el = std::to_string(i);
+        for(int m=el.length()-1 ;m>=0;m--, zeroCharCnt++) {
+            stringList.push_back(muliplicated(sRet, el[m] , zeroCharCnt));
+        }
+        zeroCharCnt=0;
+
+        for(auto ori:stringList) {
+            sSumRet = plus(sSumRet, ori);
+        }
+        stringList.clear();
+
+        sRet = sSumRet;
+        sSumRet = "0";
+    });
+}
+
+auto Bigdecimal::plus(std::string value1, std::string value2) -> std::string {
+    std::forward_list<char> ret;
+    int i=value1.size()-1;
+    int j=value2.size()-1;
+
+    std::string itos;
+    int sum, iValue, jValue, upValue;
+    upValue=0;
+    for(;; --i, --j) {
+        if(i<0 && j<0) {
+            if(upValue>0)
+                ret.push_front('1');
+            break;
+        }
+
+        if(i<0) iValue = 0;
+        else iValue = value1[i]-'0';
+
+        if(j<0) jValue = 0;
+        else jValue = value2[j]-'0';
+
+        if(upValue>0)
+            iValue+=1;
+
+        itos = std::to_string(iValue + jValue);
+        if( itos.length() > 1) {
+            upValue=1;
+            ret.push_front(itos[1]);
+        }
+        else {
+            upValue=0;
+            ret.push_front(itos[0]);
+        }
+        iValue=jValue=0;
+    }
+    return std::string(ret.begin(),ret.end());
+}
+
+auto Bigdecimal::toString() -> std::string {
+    return coreValue;
+}
+
+auto Bigdecimal::copyFrom(Bigdecimal &&rhs) noexcept -> void {
+    coreValue = rhs.coreValue;
+}
+auto Bigdecimal::copyFrom(const Bigdecimal &rhs) noexcept -> void {
+    coreValue = rhs.coreValue;
+}
+
+}//doori end
+
+
---
+++ doori_api/Bigdecimal.h
@@ -0,0 +1,65 @@
+//
+// Created by jaeseong on 21. 11. 1..
+//
+
+#ifndef DOORI_PROJECT_BIGDECIMAL_H
+#define DOORI_PROJECT_BIGDECIMAL_H
+
+#include <iostream>
+#include <forward_list>
+#include <list>
+#include <algorithm>
+
+namespace doori {
+/**
+ * C/C++ 용 Big Decimal 클래스. 아주 큰 값을 문자열방식으로 산수처리함
+ */
+class Bigdecimal{
+public:
+    /**
+     * 기본생성자 사용금지.
+     */
+    Bigdecimal() = delete;
+    /**
+     * 기본생성자.
+     * @param value
+     */
+    Bigdecimal(std::string value);
+    /**
+     * 복사생성자
+     * @param rhs
+     */
+    Bigdecimal(const Bigdecimal& rhs);
+    /**
+     * 이동생성자
+     * @param rhs
+     */
+    Bigdecimal(Bigdecimal&& rhs);
+    /**
+     * 곱하기
+     * @param rhs
+     * @return
+     */
+    auto operator*(const Bigdecimal& rhs)-> Bigdecimal&;
+    /**
+     * 더하기
+     * @param rhs
+     * @return
+     */
+    auto operator+(const Bigdecimal& rhs)-> Bigdecimal&;
+    /**
+     * 값을 문자열로 리턴 합니다.
+     * @return
+     */
+    auto toString() -> std::string;
+private:
+    auto muliplicated(std::string value, char c, uint zeroCharCnt) -> std::string;
+    auto plus(std::string value1, std::string value2) -> std::string;
+    auto copyFrom(const Bigdecimal& rhs) noexcept ->void;
+    auto copyFrom(Bigdecimal&& rhs) noexcept ->void;
+    std::string coreValue;
+};
+}
+
+
+#endif //DOORI_PROJECT_BIGDECIMAL_H
