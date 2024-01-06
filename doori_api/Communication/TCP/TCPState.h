5e9fb726fdbb254d6de245c421000856dd13e579
dooripapa <b532882@hotmail.com>
2024-01-06 10:43:25 +0900

    TCP state pattern으로 구현시작.

---
+++ doori_api/Communication/TCP/TCPState.h
@@ -0,0 +1,8 @@
+//
+// Created by jslee on 24. 1. 6.
+//
+
+#ifndef DOORI_PROJECT_TCPSTATE_H
+#define DOORI_PROJECT_TCPSTATE_H
+
+#endif //DOORI_PROJECT_TCPSTATE_H
---
+++ doori_tests/test_TCPState.cpp
@@ -0,0 +1,43 @@
+//
+// Created by jaeseong on 23. 5. 30.
+//
+
+#include <gtest/gtest.h>
+#include <thread>
+#include <functional>
+#include "Common/Log.h"
+#include "Communication/IIPCTopologyNode.h"
+#include "Communication/TCP/TCPtopologyNode.h"
+
+using namespace doori::api::Communication::TCP;
+using namespace doori::api::Communication;
+
+
+TEST(TCPState, Client)
+{
+    TCPtopologyNode tcp;
+
+    tcp.setState(new TCPinit("127.0.0.1", "8888"));
+}
+
+TEST(TCPState, Remote)
+{
+    IIPCTopologyNode tcp;
+
+    std::function<int(IIPCTopologyNode const &node)> processing;
+
+    processing = [](IIPCTopologyNode const &node) {
+
+        std::string buffer;
+        std::int32_t bufferSize = 200;
+
+        node.Recv(buffer, bufferSize);
+
+        PLOG(DEBUG, "Recv Data[%t]", buffer);
+
+        return 0;
+    };
+
+    tcp.setState(new TCPwait("127.0.0.1", "8888", processing));
+
+}
